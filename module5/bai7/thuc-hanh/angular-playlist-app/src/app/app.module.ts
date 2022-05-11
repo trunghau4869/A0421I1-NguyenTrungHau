2c61e4544062c8958ad394360c72c32fc3f964e3
trunghau4869 <83228878+trunghau4869@users.noreply.github.com>
2022-05-11 19:43:14 +0700

    update 11/5
    update 11/5

---
+++ module5/bai7/thuc-hanh/angular-playlist-app/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {YoutubePlaylistComponent} from './youtube-playlist/youtube-playlist.component';
+import {YoutubePlayerComponent} from './youtube-player/youtube-player.component';
+import {Routes} from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'youtube',
+    component: YoutubePlaylistComponent,
+    children: [{
+      path: ':id',
+      component: YoutubePlayerComponent
+    }]
+  }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    YoutubePlaylistComponent,
+    YoutubePlayerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ module5/bai7/thuc-hanh/angular-product-management/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductCreateComponent } from './product-create/product-create.component';
+import { ProductListComponent } from './product-list/product-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductCreateComponent,
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
